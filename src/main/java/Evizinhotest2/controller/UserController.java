f801805706488f5f7cb9bec8788b3c77d35c98b6
Weverson <weversonpsf@gmail.com>
2021-03-14 20:17:49 -0300

    tratamento de exceção em register

--- src/main/java/Evizinhotest2/controller/UserController.java
+++ src/main/java/Evizinhotest2/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import Evizinhotest2.model.User;
 import Evizinhotest2.service.UserService;
@@ -17,8 +17,8 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	private static final String MSG_SUCESS_ADD = "Post Adicionado com Sucesso!";
-	private static final String MSG_SUCESS_UPDATE = "Post Atualizado com Sucesso!";
+	private static final String MSG_SUCESS_ADD = "Usuário cadastrado com Sucesso!";
+	private static final String MSG_SUCESS_UPDATE = "Usuário Atualizado com Sucesso!";
 	private static final String MSG_SUCESS_DELETE = "Excluído com sucesso!.";
 	private static final String MSG_ERROR = "Error.";
 	
@@ -48,7 +48,7 @@ public class UserController {
 
 	 @GetMapping("/users/{id}")
 	 public String show(Model model, @PathVariable("id") Integer id) {
-	 	if (id != null) {
+		 if (id != null) {
 	 		User user = userService.getUser(id).get();
 	 		model.addAttribute("user", user);
 	 	}
@@ -56,15 +56,35 @@ public class UserController {
 	 }
 
 	@RequestMapping(value = "/users/form", method=RequestMethod.GET)
-	public String register(Model model) {
-	 	User user = new User();
-	 	model.addAttribute("user", user);
+	public String register(Model model, RedirectAttributes redirectAttributes) {
+		try {
+			User user = new User();
+			model.addAttribute("user", user);
+		}catch (Exception e) {
+			System.out.println("Exception:: exception");
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("error", MSG_ERROR);
+		}catch (Throwable e) {
+			System.out.println("Throwable:: exception");
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("error", MSG_ERROR);
+		}
 		return "register";
 	}
 
 	@RequestMapping(value = "/users/register", method=RequestMethod.POST)
-	public String register(User user) {
-		userService.addUser(user);
+	public String register(User user, RedirectAttributes redirectAttributes) {
+		try {
+			userService.addUser(user);
+		}catch (Exception e) {
+			System.out.println("Exception:: exception");
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("error", MSG_ERROR);
+		}catch (Throwable e) {
+			System.out.println("Throwable:: exception");
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("error", MSG_ERROR);
+		}
 		return "home";
 	}
 
